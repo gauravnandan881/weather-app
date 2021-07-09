5421a010ea95e4f3930ba1ec1175bcc07406c41f
deadpool881 <33416351+deadpool881@users.noreply.github.com>
2021-07-09 16:45:39 +0530

    Update weather-service.component.ts

--- src/app/weather-service/weather-service.component.ts
+++ src/app/weather-service/weather-service.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+mport { Injectable } from '@angular/core';
+import { WEATHER_ITEMS } from './weather.data';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs/internal/Observable';
+import { WeatherItem } from './weather-item';
+import { map } from 'rxjs/Operators';
+
 @Component({
   selector: 'app-weather-service',
   templateUrl: './weather-service.component.html',
   styleUrls: ['./weather-service.component.css']
 })
-export class WeatherServiceComponent implements OnInit {
-  constructor(private http:HttpClient) {}
-  ngOnInit(): void {
-  }
-  // getWeatherItems(){
-  //   return WEATHER_ITEMS;
-  // }
-  // searchWeatherData(cityName : string) : Observable<any> {
-  //   // return this.http.
-  // }
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherServiceComponent {
+  constructor(private http: HttpClient) { }
+    getWeatherItems() {
+      return WEATHER_ITEMS;
+    }
+    addWeatherItem(weatherItem: WeatherItem) {
+      WEATHER_ITEMS.push(weatherItem)
+    }
+    clearWeatherItems() {
+      WEATHER_ITEMS.splice(0);
+    }
+    searchWeatherData(cityName: string): Observable<any> {
+    return this.http
+      .get('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid={a5090bb223459ed6646f79796aa72d9b}')
+      .pipe(map(response => console.log(response)))
+    }
 }
